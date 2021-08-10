5eca7711abe82a892ecc78b3a38b9c9f1e3d9ba0
Mayank19Joshi <mayankjoshi12310@gmail.com>
2021-08-10 22:04:16 +0530

    .

--- week-0/binary_search.cpp
+++ week-0/binary_search.cpp
@@ -1,40 +1,39 @@
 #include<iostream>
 using namespace std;
-int binsearch(int arr[],int F,int L,int ele)
-{
-	if(F<L)
-	{
-		int mid = (F+L)/2;
-		if(arr[mid] == ele)
-		return mid;
-		else if(arr[mid]>ele)
-		binsearch(arr,F,mid-1,ele);
-		else if(arr[mid]<ele)
-		binsearch(arr,mid+1,L,ele);
-	}
-	else if(F==L)
-	return -1;
+int binarySearch(int arr[], int p, int r, int num) {
+   if (p <= r) {
+      int mid = (p + r)/2;
+      if (arr[mid] == num)
+         return mid ;
+      if (arr[mid] > num)
+         return binarySearch(arr, p, mid-1, num);
+      if (arr[mid] < num)
+         return binarySearch(arr, mid+1, r, num);
+   }
+   return -1;
 }
-int main()
-{
-	int n,i,ele;
-	int arr[100];
-	cout<<"Enter the number of elements"<<endl;
-	cin>>n;
-	cout<<"Enter the elements of array"<<endl;
-	while(i<n)
-	{
-		cin>>arr[i];
-		i++;
-	}
-	cout<<"Enter the element to be searched"<<endl;
-	cin>>ele;
-	n = binsearch(arr,0,n,ele);
-	if(n == -1)
-	{
-		cout<<"Element not found"<<endl;
-		return 0;
-	}
-	cout<<"Element "<<ele<<" found at index "<<n<<endl;;
-	return 0;
+int main(void) {
+   int arr[20],n,i,num;
+   for(int j=0;j<=2;j++)
+   {
+       cout<<"Enter the number of elements"<<endl;
+       cin>>n;
+       cout<<"Enter the elements of array"<<endl;
+       for(i=0;i<=n-1;i++)
+       {
+           cin>>arr[i];
+       }
+       cout << "Enter the number to search: \n";
+       cin >> num;
+       int index = binarySearch (arr, 0, n-1, num);
+       if(index == -1)
+       {
+           cout<< num <<" is not present in the array"<<endl;
+       }
+       else
+       {
+           cout<< num <<" is present at index "<< index <<" in the array"<<endl;
+       }
+   }
+    return 0;
 }
